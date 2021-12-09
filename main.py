c04bbc89ac03286c5c4441163f3335217c634788
Jonathan Burman <jonathan.burman@timeout.com>
2021-12-09 17:15:10 +0000

    update tests and impletement remaining functionality

--- main.py
+++ main.py
@@ -23,22 +23,38 @@ def readfile(file):
 
         return results
 
-def send_email():
+def send_email(students):
     SMTP_SERVER = os.environ.get('SMTP_SERVER', 'smtp.gmail.com')
     EMAIL_LOGIN = os.environ.get('EMAIL_LOGIN')
     EMAIL_PASS = os.environ.get('EMAIL_PASS')
 
-    server = smtplib.SMTP_SSL(SMTP_SERVER, 465)
-    server.login(EMAIL_LOGIN, EMAIL_PASS)
-    server.sendmail(
-        "test@test.co.uk", 
-        "test.send@example.com", 
-        "this message is from python")
-    server.quit()
+    presenting_student = choose_random_student(students)
+
+    for student in students:
+
+        message = f"""\
+Dear {students[student]['first_name']}, Your score for the book assignment is broken down below by question number.
+
+{students[student]['problem1_score']}%: {students[student]['problem1_comments']}
+{students[student]['problem2_score']}%: {students[student]['problem2_comments']}
+{students[student]['problem3_score']}%: {students[student]['problem3_comments']}
+
+"""
+        if presenting_student == student:
+            message += "You've been randomly chosen to present a summary of the book in the next class. Looking forward to it!"
+
+        server = smtplib.SMTP_SSL(SMTP_SERVER, 465)
+        server.login(EMAIL_LOGIN, EMAIL_PASS)
+        server.sendmail(
+            'lecture@example.com',
+            student,
+            message
+            )
+        server.quit()
     
 def choose_random_student(results):
     return random.choice(list(results.keys()))
 
 if __name__ == '__main__':
-    readfile("./exam.csv")
-    send_email()
+    student_list = readfile("./exam.csv")
+    send_email(student_list)
--- tests.py
+++ tests.py
@@ -5,34 +5,82 @@ from io import StringIO
 
 from main import choose_random_student, readfile, send_email
 
+def mock_csv_data():
+    return """\
+a@a.com,Bell,Ana,60,good,70,better,80,best
+b@b.com,Pop,Banana,10,worst,20,bad,30,ok"""
+
+class TestAcceptanceTest(TestCase):
+    def test_read_in_csv_and_send_email(self):
+
+        # First read in CSV file
+
+        with patch("builtins.open", mock_open(read_data=mock_csv_data())) as mock_file:
+            student_list = readfile('/path/to/open')
+
+            with mock.patch.dict('os.environ', {
+            'EMAIL_LOGIN': 'test@test.co.uk',
+            'EMAIL_PASS': 'password'
+            }):
+                with patch('smtplib.SMTP_SSL', autospec=True) as mock_smtp:
+                    with patch('main.choose_random_student') as mock_choose_random_student:
+                        mock_choose_random_student.return_value = 'b@b.com'
+
+                        send_email(student_list)
+
+                        instance = mock_smtp.return_value
+                        
+                        self.assertEqual(instance.login.call_count, 2)
+                        self.assertEqual(instance.sendmail.call_count, 2)
+                        self.assertEqual(instance.quit.call_count, 2)
+
+                        self.assertEqual(instance.sendmail.mock_calls[0][1][0], 'lecture@example.com')
+                        self.assertEqual(instance.sendmail.mock_calls[0][1][1], 'a@a.com')
+                        self.assertIn('Dear Bell', instance.sendmail.mock_calls[0][1][2])
+                        self.assertNotIn('You\'ve been randomly', instance.sendmail.mock_calls[0][1][2])
+
+                        self.assertEqual(instance.sendmail.mock_calls[1][1][0], 'lecture@example.com')
+                        self.assertEqual(instance.sendmail.mock_calls[1][1][1], 'b@b.com')
+                        self.assertIn('Dear Pop', instance.sendmail.mock_calls[1][1][2])
+                        self.assertIn('You\'ve been randomly', instance.sendmail.mock_calls[1][1][2])
+
+
 class TestCSV(TestCase):
 
-    def mock_csv_data(self):
-        return """\
-a@a.a,Bell,Ana,60,good,70,better,80,best
-b@b.b,Pop,Banana,10,worst,20,bad,30,ok"""
-    
     def test_can_read_csv(self):  
-        with patch("builtins.open", mock_open(read_data=self.mock_csv_data())) as mock_file:
+        with patch("builtins.open", mock_open(read_data=mock_csv_data())) as mock_file:
             readfile("path/to/open")
         mock_file.assert_called_with("path/to/open", "r")
 
     def test_read_line_to_dictionary(self):
-        with patch("builtins.open", mock_open(read_data=self.mock_csv_data())) as mock_file:
+        with patch("builtins.open", mock_open(read_data=mock_csv_data())) as mock_file:
             students = readfile("path/to/open")
-        self.assertIn("a@a.a", students)
+        self.assertIn("a@a.com", students)
 
 
 class TestEmail(TestCase):
 
     def test_send_test_email(self):
+
+        student_results = {
+            'test.send@example.com': {
+                'first_name': 'test_first',
+                'last_name':  'test_last',
+                'problem1_score': 34,
+                'problem1_comments': 'bad',
+                'problem2_score': 60,
+                'problem2_comments': 'good',
+                'problem3_score': 90,
+                'problem3_comments': 'great',
+            }
+        }
         
         with mock.patch.dict('os.environ', {
             'EMAIL_LOGIN': 'test@test.co.uk',
             'EMAIL_PASS': 'password'
             }):
             with patch('smtplib.SMTP_SSL', autospec=True) as mock_smtp:
-                send_email()
+                send_email(student_results)
 
                 instance = mock_smtp.return_value
                 
@@ -44,9 +92,9 @@ class TestEmail(TestCase):
                 self.assertTrue(instance.sendmail.called)
                 self.assertEqual(instance.sendmail.call_count, 1)
                 
-                self.assertEqual(instance.sendmail.mock_calls[0][1][0], 'test@test.co.uk')
+                self.assertEqual(instance.sendmail.mock_calls[0][1][0], 'lecture@example.com')
                 self.assertEqual(instance.sendmail.mock_calls[0][1][1], 'test.send@example.com')
-                self.assertEqual(instance.sendmail.mock_calls[0][1][2], 'this message is from python')
+                self.assertIn('Dear test_first', instance.sendmail.mock_calls[0][1][2])
                 
                 self.assertTrue(instance.quit.called)
                 self.assertEqual(instance.quit.call_count, 1)
