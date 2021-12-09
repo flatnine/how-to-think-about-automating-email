4019897c4962b35a2d60ace274fc2e986500d6eb
Jonathan Burman <jonathan.burman@timeout.com>
2021-12-09 15:31:57 +0000

    add send_email function and choose_random_student

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
 #!/usr/bin/env python
 import csv
+import smtplib 
+import random
+import os 
+
 
 def readfile(file):
     with open(file, 'r') as f:
+        results = {}
         for row in csv.reader(f):
+            results[row[0]] = {
+                'first_name': row[1],
+                'last_name':  row[2],
+                'problem1_score': row[3],
+                'problem1_comments': row[4],
+                'problem2_score': row[5],
+                'problem2_comments': row[6],
+                'problem3_score': row[7],
+                'problem3_comments': row[8],
+            }
             print(row)
 
+        return results
+
+def send_email():
+    SMTP_SERVER = os.environ.get('SMTP_SERVER', 'smtp.gmail.com')
+    EMAIL_LOGIN = os.environ.get('EMAIL_LOGIN')
+    EMAIL_PASS = os.environ.get('EMAIL_PASS')
+
+    server = smtplib.SMTP_SSL(SMTP_SERVER, 465)
+    server.login(EMAIL_LOGIN, EMAIL_PASS)
+    server.sendmail(
+        "test@test.co.uk", 
+        "test.send@example.com", 
+        "this message is from python")
+    server.quit()
+    
+def choose_random_student(results):
+    return random.choice(list(results.keys()))
 
 if __name__ == '__main__':
     readfile("./exam.csv")
+    send_email()
--- tests.py
+++ tests.py
@@ -1,18 +1,65 @@
 from unittest import TestCase
+from unittest import mock
 from unittest.mock import patch, mock_open
 from io import StringIO
 
-from main import readfile
+from main import choose_random_student, readfile, send_email
 
 class TestCSV(TestCase):
 
     def mock_csv_data(self):
         return """\
-Day,MxT,MnT,AvT,AvDP,1HrP TPcn,PDir,AvSp,Dir,MxS,SkyC,MxR,Mn,R AvSLP\n
-1,88,59,74,53.8,0,280,9.6,270,17,1.6,93,23,1004.5
-2,79,63,71,46.5,0,330,8.7,340,23,3.3,70,28,1004.5"""
+a@a.a,Bell,Ana,60,good,70,better,80,best
+b@b.b,Pop,Banana,10,worst,20,bad,30,ok"""
     
     def test_can_read_csv(self):  
         with patch("builtins.open", mock_open(read_data=self.mock_csv_data())) as mock_file:
             readfile("path/to/open")
         mock_file.assert_called_with("path/to/open", "r")
+
+    def test_read_line_to_dictionary(self):
+        with patch("builtins.open", mock_open(read_data=self.mock_csv_data())) as mock_file:
+            students = readfile("path/to/open")
+        self.assertIn("a@a.a", students)
+
+
+class TestEmail(TestCase):
+
+    def test_send_test_email(self):
+        
+        with mock.patch.dict('os.environ', {
+            'EMAIL_LOGIN': 'test@test.co.uk',
+            'EMAIL_PASS': 'password'
+            }):
+            with patch('smtplib.SMTP_SSL', autospec=True) as mock_smtp:
+                send_email()
+
+                instance = mock_smtp.return_value
+                
+                self.assertTrue(instance.login.called)
+                self.assertEqual(instance.login.call_count, 1)
+                self.assertEqual(instance.login.mock_calls[0][1][0], 'test@test.co.uk')
+                self.assertEqual(instance.login.mock_calls[0][1][1], 'password')
+
+                self.assertTrue(instance.sendmail.called)
+                self.assertEqual(instance.sendmail.call_count, 1)
+                
+                self.assertEqual(instance.sendmail.mock_calls[0][1][0], 'test@test.co.uk')
+                self.assertEqual(instance.sendmail.mock_calls[0][1][1], 'test.send@example.com')
+                self.assertEqual(instance.sendmail.mock_calls[0][1][2], 'this message is from python')
+                
+                self.assertTrue(instance.quit.called)
+                self.assertEqual(instance.quit.call_count, 1)
+           
+    def test_choose_random_user(self):
+
+        student_list = {
+            'a@a.com': {},
+            'b@b.com': {},
+            'c@c.com': {},
+        }
+        student = choose_random_student(student_list)
+        self.assertIn(student, student_list)
+        
+           
+         
